869e60ae1803d61318762d5ca6aea2b58823ea32
Frostburn (James) <tonjesjames@gmail.com>
2024-11-12 21:23:37 +0100

    Changed Min/Max from std::max or std::min to a ternary operator based function in paddle.cpp

--- HelloWorld/paddle.cpp
+++ HelloWorld/paddle.cpp
@@ -1,5 +1,13 @@
 #include "paddle.h"
 //paddleHeight and paddleWidth are half the actual value, measured from the origin
+
+float Max(float x, float y) {
+	return (x > y) ? x : y;
+}
+float Min(float x, float y) {
+	return (x < y) ? x : y;
+}
+
 void DrawPaddle(const Paddle& paddle) {
 	Play::Vector2D bl = { paddle.pos.x - paddleLen, paddle.pos.y - paddleHeight };
 	Play::Vector2D tr = { paddle.pos.x + paddleLen, paddle.pos.y + paddleHeight };
@@ -7,16 +15,16 @@ void DrawPaddle(const Paddle& paddle) {
 }
 //sets the paddle origin point to it's self, or the edge of the window 
 void UpdatePaddle(Paddle& paddle) {
-		
-	paddle.pos.x = std::max((paddle.pos.x - paddleSpeed * (Play::KeyDown(Play::KEY_LEFT))), 0.0f);
-	paddle.pos.x = std::min((paddle.pos.x + paddleSpeed * (Play::KeyDown(Play::KEY_RIGHT))), float(DISPLAY_WIDTH));
+
+	paddle.pos.x = Max((paddle.pos.x - paddleSpeed * (Play::KeyDown(Play::KEY_LEFT))), 0.0f);
+	paddle.pos.x = Min((paddle.pos.x + paddleSpeed * (Play::KeyDown(Play::KEY_RIGHT))), float(DISPLAY_WIDTH));
 }
 
 bool IsColliding(const Paddle& paddle, const Play::GameObject& obj) {
-	Play::Vector2D paddleTopLeft = { (paddle.pos.x - paddleLen), (paddle.pos.y + paddleHeight)};
+	Play::Vector2D paddleTopLeft = { (paddle.pos.x - paddleLen), (paddle.pos.y + paddleHeight) };
 	Play::Vector2D paddleBottomRight = { (paddle.pos.x + paddleLen), (paddle.pos.y - paddleHeight) };
 
-	const float dx = obj.pos.x - std::max(paddleTopLeft.x, std::min(obj.pos.x, paddleBottomRight.x));
-	const float dy = obj.pos.y - std::max(paddleTopLeft.y, std::min(obj.pos.y, paddleBottomRight.y));
+	const float dx = obj.pos.x - Max(paddleTopLeft.x, Min(obj.pos.x, paddleBottomRight.x));
+	const float dy = obj.pos.y - Max(paddleTopLeft.y, Min(obj.pos.y, paddleBottomRight.y));
 	return (dx * dx + dy * dy) < (obj.radius * obj.radius);
 }
